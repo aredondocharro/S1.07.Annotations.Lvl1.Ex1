d747889b2a9a070f9b33a25f5f529788f6d0c9d9
Alejandro Redondo Charro <alejandro.redondo.charro@gmail.com>
2025-04-23 13:22:34 +0200

    Minor changes

--- src/main/java/OnlineWorker.java
+++ src/main/java/OnlineWorker.java
@@ -1,5 +1,5 @@
 public class OnlineWorker extends Worker {
-    private static final double INTERNET_FLAT_RATE = 30.0;
+    private static final double internetFlatPrice = 30.0;
 
     public OnlineWorker(String name, String surname, double hourlyRate) {
         super(name, surname, hourlyRate);
@@ -7,6 +7,6 @@ public class OnlineWorker extends Worker {
 
     @Override
     public double calculateSalary(int hoursWorked) {
-        return super.getHourlyRate() * hoursWorked + INTERNET_FLAT_RATE;
+        return super.getHourlyRate() * hoursWorked + internetFlatPrice;
     }
 }
